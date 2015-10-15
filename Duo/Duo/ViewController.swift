2562541e7210d2fc3d2a43d165569f39869ac00c
Boris-Em <boris.emorine@gmail.com>
2015-10-14 23:53:19 -0700

    Created Xcode project

---
+++ Duo/Duo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Duo
+//
+//  Created by Bobo on 10/14/15.
+//  Copyright © 2015 Boris Emorine. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
