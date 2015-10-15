4a01558833d052f56809fdd7317ea48310e81cce
Boris-Em <boris.emorine@gmail.com>
2015-10-15 10:10:45 -0700

    Created NSManagedObject subclasses

---
+++ Duo/Duo/Question+CoreDataProperties.swift
@@ -0,0 +1,25 @@
+//
+//  Question+CoreDataProperties.swift
+//  Duo
+//
+//  Created by Bobo on 10/15/15.
+//  Copyright © 2015 Boris Emorine. All rights reserved.
+//
+//  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
+//  to delete and recreate this implementation file for your updated model.
+//
+
+import Foundation
+import CoreData
+
+extension Question {
+
+    @NSManaged var createdAt: NSDate?
+    @NSManaged var imgURLs: NSObject?
+    @NSManaged var myVote: NSNumber?
+    @NSManaged var questionId: String?
+    @NSManaged var textBody: String?
+    @NSManaged var votes: NSObject?
+    @NSManaged var createdBy: NSManagedObject?
+
+}
---
+++ Duo/Duo/User.swift
@@ -0,0 +1,16 @@
+//
+//  User.swift
+//  Duo
+//
+//  Created by Bobo on 10/15/15.
+//  Copyright © 2015 Boris Emorine. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class User: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
