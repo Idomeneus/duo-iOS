3472fa5be3d065741cf7220a0cddea1a60ef20f4
Boris-Em <boris.emorine@gmail.com>
2015-10-15 09:00:04 -0700

    Created new MainFeedTableViewController

---
+++ Duo/Duo/MainFeedTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MainFeedTableViewController.swift
+//  Duo
+//
+//  Created by Bobo on 10/15/15.
+//  Copyright © 2015 Boris Emorine. All rights reserved.
+//
+
+import UIKit
+
+class MainFeedTableViewController: UITableViewController {
+
+}
