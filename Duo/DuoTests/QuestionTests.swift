a412ea3d41a37a811d5e504e15151597367b48ee
Boris-Em <boris.emorine@gmail.com>
2015-10-15 21:49:13 -0700

    Added tests for Question

--- Duo/DuoTests/QuestionTests.swift
+++ Duo/DuoTests/QuestionTests.swift
@@ -60,7 +60,7 @@ class QuestionTests: XCTestCase {
             XCTAssert(user.userId == "usr:2418905")
             XCTAssert(user.userName == "user1")
             
-
+            
             // Test Second Valid Dictionary
             questionJSON = questionsJSON[1]
             question = Question(dict: questionJSON, insertIntoManagedObjectContext: moc)
@@ -117,7 +117,134 @@ class QuestionTests: XCTestCase {
             XCTAssertNil(question.imgURLs)
         }
         catch {
+            XCTFail()
             print("Error getting dictionary")
         }
+    }
+    
+    func testGetQuestionWithId() {
+        
+        // No questionId
+        var question: Question? = Question.getQuestionWithId("", inManageObjectContext: moc)
+        XCTAssertNil(question)
+        
+        // Make sure that the question is not already in our moc
+        question = Question.getQuestionWithId("qst:58432098", inManageObjectContext: moc)
+        XCTAssertNil(question)
+        
+        let path: String? = NSBundle(forClass: self.classForCoder).pathForResource("MockedQuestions", ofType: "json")
+        let jsonData = NSData(contentsOfFile: path!)
+        let dict: Dictionary<String, AnyObject>?
+        
+        do {
+            dict = try NSJSONSerialization.JSONObjectWithData(jsonData!, options: NSJSONReadingOptions.MutableContainers) as? Dictionary<String, AnyObject>
+            let questionsJSON: [[String: AnyObject]] = dict!["list"] as! [[String: AnyObject]]
+            let questionJSON = questionsJSON[0]
+
+            question = Question(dict: questionJSON, insertIntoManagedObjectContext: moc)
+            XCTAssertNotNil(question)
+            
+            question = nil
+            XCTAssertNil(question)
+            
+            question = Question.getQuestionWithId("qst:58432098", inManageObjectContext: moc)
+            XCTAssertNotNil(question)
+            XCTAssert(question!.questionId == "qst:58432098")
+            XCTAssert(question!.textBody == "Who is better?")
+            XCTAssert(question!.createdAt!.isEqualToDate(NSDate(timeIntervalSince1970: 1444925251)))
+            XCTAssertNil(question!.myVote)
+            
+            let votes: [Int] = question!.votes as! [Int]
+            XCTAssert(votes[0] == 77532)
+            XCTAssert(votes[1] == 58421)
+            
+            let imageURLs: [String] = question!.imgURLs as! [String]
+            XCTAssert(imageURLs[0] == "https://upload.wikimedia.org/wikipedia/en/0/0a/Justin_brandon_guitar.jpg")
+            XCTAssert(imageURLs[1] == "https://upload.wikimedia.org/wikipedia/en/d/d2/Justindcooper.jpg")
+            
+            let user: User = question!.createdBy as! User
+            XCTAssert(user.userId == "usr:2418905")
+            XCTAssert(user.userName == "user1")
+        } catch {
+            XCTFail()
+            print("Error getting dictionary")
+        }
+    }
+    
+    func testExistingOrNewQuestion() {
+        
+        // Question with empty dictionary
+        var question: Question? = Question.existingOrNewQuestionWithDictionary(["": ""], inManageObjectContext: moc)
+        XCTAssertNotNil(question)
+        
+        let path: String? = NSBundle(forClass: self.classForCoder).pathForResource("MockedQuestions", ofType: "json")
+        let jsonData = NSData(contentsOfFile: path!)
+        let dict: Dictionary<String, AnyObject>?
+        
+        do {
+            dict = try NSJSONSerialization.JSONObjectWithData(jsonData!, options: NSJSONReadingOptions.MutableContainers) as? Dictionary<String, AnyObject>
+            let questionsJSON: [[String: AnyObject]] = dict!["list"] as! [[String: AnyObject]]
+            let questionJSON = questionsJSON[0]
+            
+            
+            // New question with dictionary
+            question = Question.existingOrNewQuestionWithDictionary(questionJSON, inManageObjectContext: moc)
+            XCTAssertNotNil(question)
+            XCTAssert(question!.questionId == "qst:58432098")
+            XCTAssert(question!.textBody == "Who is better?")
+            XCTAssert(question!.createdAt!.isEqualToDate(NSDate(timeIntervalSince1970: 1444925251)))
+            XCTAssertNil(question!.myVote)
+            
+            var votes: [Int] = question!.votes as! [Int]
+            XCTAssert(votes[0] == 77532)
+            XCTAssert(votes[1] == 58421)
+            
+            var imageURLs: [String] = question!.imgURLs as! [String]
+            XCTAssert(imageURLs[0] == "https://upload.wikimedia.org/wikipedia/en/0/0a/Justin_brandon_guitar.jpg")
+            XCTAssert(imageURLs[1] == "https://upload.wikimedia.org/wikipedia/en/d/d2/Justindcooper.jpg")
+            
+            var user: User = question!.createdBy as! User
+            XCTAssert(user.userId == "usr:2418905")
+            XCTAssert(user.userName == "user1")
+            
+            
+            // Sets the question to nil
+            question = nil
+            XCTAssertNil(question)
+            
+            
+            // Retrieve the question with its questionId
+            question = Question.existingOrNewQuestionWithId("qst:58432098", inManageObjectContext: moc)
+            XCTAssertNotNil(question)
+            XCTAssert(question!.questionId == "qst:58432098")
+            XCTAssert(question!.textBody == "Who is better?")
+            XCTAssert(question!.createdAt!.isEqualToDate(NSDate(timeIntervalSince1970: 1444925251)))
+            XCTAssertNil(question!.myVote)
+            
+            votes = question!.votes as! [Int]
+            XCTAssert(votes[0] == 77532)
+            XCTAssert(votes[1] == 58421)
+            
+            imageURLs = question!.imgURLs as! [String]
+            XCTAssert(imageURLs[0] == "https://upload.wikimedia.org/wikipedia/en/0/0a/Justin_brandon_guitar.jpg")
+            XCTAssert(imageURLs[1] == "https://upload.wikimedia.org/wikipedia/en/d/d2/Justindcooper.jpg")
+            
+            user = question!.createdBy as! User
+            XCTAssert(user.userId == "usr:2418905")
+            XCTAssert(user.userName == "user1")
+        } catch {
+            XCTFail()
+            print("Error getting dictionary")
+        }
+        
+        // New Question with new questionId
+        question = Question.existingOrNewQuestionWithId("unknownId", inManageObjectContext: moc)
+        XCTAssertNotNil(question)
+        XCTAssert(question!.questionId == "unknownId")
+        XCTAssertNil(question!.textBody)
+        XCTAssertNil(question!.createdAt)
+        XCTAssertNil(question!.createdBy)
+        XCTAssertNil(question!.myVote)
+        XCTAssertNil(question!.votes)
     }
 }
