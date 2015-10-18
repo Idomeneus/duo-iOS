1ca258204e15abffd0e87ec91a4f9be22f799933
Boris-Em <boris.emorine@gmail.com>
2015-10-17 21:14:40 -0700

    Added simple networking functions for API calls

---
+++ Duo/Duo/QuestionsService.swift
@@ -0,0 +1,69 @@
+//
+//  QuestionsService.swift
+//  Duo
+//
+//  Created by Bobo on 10/17/15.
+//  Copyright © 2015 Boris Emorine. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+struct QuestionsService {
+    
+    static func getQuestionsForCategory(category: Enums.questionsCategory, inManagedObjectContext context:NSManagedObjectContext, withCompletion completion:((questions: List?, error: NSError?) -> Void)?) {
+        
+        getQuestionsDictionaryForCategory(category) { (questionsDict, error) -> Void in
+            if (error != nil) {
+                completion?(questions: nil, error: error!)
+                return
+            }
+            
+            let questions: List = List(dict: questionsDict!, insertIntoManagedObjectContext: context)
+            
+            completion?(questions: questions, error: nil)
+            return
+        }
+    }
+    
+    static func getQuestionsDictionaryForCategory(category: Enums.questionsCategory, withCompletion completion: ((questionsDict: [String: AnyObject]?, error: NSError?) -> Void)?) {
+        
+        let URL: NSURL = Router.questionsURLWithParameters(nil)
+        
+        let session = NSURLSession.sharedSession()
+        
+        let task = session.dataTaskWithURL(URL) { (data, response, error) -> Void in
+            
+            if (error != nil) {
+                completion?(questionsDict: nil, error: error)
+                return
+            }
+            
+            let response: NSHTTPURLResponse? = response as? NSHTTPURLResponse
+            
+            if (response != nil && data != nil) {
+                if (response!.statusCode == 200) {
+                    if let dict = try? NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.AllowFragments) as? [String: AnyObject] {
+                        completion?(questionsDict: dict, error: nil)
+                        return
+                    } else {
+                        completion?(questionsDict: nil, error: NSError(domain: "An unexpected error occured", code: 0, userInfo: nil))
+                        return
+                    }
+                } else {
+                    if let dict = try? NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.AllowFragments) as? [String: AnyObject] {
+                        completion?(questionsDict: nil, error: NSError(domain: dict!["message"] as! String, code: 0, userInfo: nil))
+                        return
+                    } else {
+                        completion?(questionsDict: nil, error: NSError(domain: "An unexpected error occured", code: 0, userInfo: nil))
+                        return
+                    }
+                }
+            } else {
+                completion?(questionsDict: nil, error: NSError(domain: "An unexpected error occured", code: 0, userInfo: nil))
+                return
+            }
+        }
+        task.resume()
+    }
+}
