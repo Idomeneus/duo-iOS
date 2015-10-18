16f6163e327791a7efb9dfca4c2fa29eb75eabf9
Boris-Em <boris.emorine@gmail.com>
2015-10-17 17:38:01 -0700

    Added new questionsCategory enum

---
+++ Duo/Duo/Enums.swift
@@ -0,0 +1,18 @@
+//
+//  Enums.swift
+//  Duo
+//
+//  Created by Bobo on 10/17/15.
+//  Copyright © 2015 Boris Emorine. All rights reserved.
+//
+
+class Enums {
+    
+    enum questionsCategory {
+        case Top
+        case Hot
+        case New
+        case Main
+    }
+
+}
