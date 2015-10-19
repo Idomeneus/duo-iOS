40cb55296d2b5038bbaf4c1dbd1c1684695efdf8
Boris-Em <boris.emorine@gmail.com>
2015-10-18 19:54:24 -0700

    Added new ErrorHandler struct

---
+++ Duo/Duo/ErrorHandler.swift
@@ -0,0 +1,17 @@
+//
+//  ErrorHandler.swift
+//  Duo
+//
+//  Created by Bobo on 10/18/15.
+//  Copyright © 2015 Boris Emorine. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct ErrorHandler {
+    
+    static func handleError(error: NSError, forViewController viewController: UIViewController) {
+        
+    }
+}
