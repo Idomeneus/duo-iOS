41a241fa170bba385087a57cd31cdf453316010b
Boris-Em <boris.emorine@gmail.com>
2015-10-18 19:44:12 -0700

    First step at implementing MainFeedTableViewController

--- Duo/Duo/MainFeedTableViewController.swift
+++ Duo/Duo/MainFeedTableViewController.swift
@@ -7,7 +7,45 @@
 //
 
 import UIKit
+import CoreData
 
 class MainFeedTableViewController: UITableViewController {
-
+    
+    var managedObjectContext: NSManagedObjectContext?
+    
+    var currentList: List?
+    
+    // MARK: Life cycle
+    override func viewDidLoad() {
+        
+        currentList = List.existingOrNewListWithName("Hot", inManageObjectContext: managedObjectContext)
+        
+        refreshData()
+    }
+    
+    private func refreshData() {
+        QuestionsService.getQuestionsForCategory(Enums.questionsCategory.Hot, inManagedObjectContext: managedObjectContext!) { (questions, error) -> Void in
+            if (error == nil && questions != nil) {
+                self.currentList = questions
+                self.tableView.reloadData()
+            } else {
+            }
+        }
+    }
+    
+    // MARK: UITableViewDataSource
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return currentList!.questions!.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "questionCell")
+        
+        return cell
+    }
 }
