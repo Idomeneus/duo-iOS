01616f1d990644956db7ed42f646f57d8809e6aa
Boris-Em <boris.emorine@gmail.com>
2015-10-18 20:20:29 -0700

    Added simple error handling

--- Duo/Duo/ErrorHandler.swift
+++ Duo/Duo/ErrorHandler.swift
@@ -12,6 +12,27 @@ import UIKit
 struct ErrorHandler {
     
     static func handleError(error: NSError, forViewController viewController: UIViewController) {
-        
+        switch error.code   {
+        case 403:
+            let alertVC: UIAlertController = UIAlertController(title: "Error", message: error.localizedDescription, preferredStyle: UIAlertControllerStyle.Alert)
+            let okayAction: UIAlertAction = UIAlertAction(title: "Login", style: UIAlertActionStyle.Default, handler: nil)
+            let cancelAction: UIAlertAction = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Cancel, handler: { (alertAction) -> Void in
+                // TODO: Present login VC
+            })
+            alertVC.addAction(okayAction)
+            alertVC.addAction(cancelAction)
+            viewController.presentViewController(alertVC, animated: true, completion: nil)
+            
+            break
+            
+        default:
+            let alertVC: UIAlertController = UIAlertController(title: "Error", message: error.localizedDescription, preferredStyle: UIAlertControllerStyle.Alert)
+            let okayAction: UIAlertAction = UIAlertAction(title: "Okay", style: UIAlertActionStyle.Default, handler: nil)
+            alertVC.addAction(okayAction)
+            
+            viewController.presentViewController(alertVC, animated: true, completion: nil)
+            
+            break
+        }
     }
 }
